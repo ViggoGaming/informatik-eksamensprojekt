39e5a57bf37c4ea6de6bbd928d556aa851900905
Victor Buch-Hansen <me@victorbuch.dk>
2023-04-29 22:08:36 +0200

    added a sqlite3 database

--- app.py
+++ app.py
@@ -9,12 +9,30 @@ from dotenv import load_dotenv
 import os
 import openai
 import nmap
+import sqlite3
+
+conn = sqlite3.connect("data.db")
+cursor = conn.cursor()
+
+# Create a table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS user_data (
+    public_ip TEXT PRIMARY KEY,
+    user_agent TEXT,
+    country TEXT,
+    open_ports TEXT,
+    descriptions TEXT,
+    banners TEXT
+)
+""")
+conn.commit()
 
 tab1, tab2, tab3, tab4 = st.tabs(["IP data", "Port scanner", "Kodeord", "Om"])
 
 # Get the user's IP address
 response = requests.get('https://api.ipify.org')
 public_ip_address = response.text
+user_agent = None
 
 # Get the user's country based on their IP address
 response = requests.get(f'https://ipapi.co/{public_ip_address}/country/')
@@ -31,6 +49,34 @@ openai.api_key = os.getenv("openai_key")
 # Get the country name based on the country code
 country_name = pycountry.countries.get(alpha_2=country_code).name
 
+def save_data_to_database(ip, user_agent, country, open_ports, descriptions, banners):
+    cursor.execute("SELECT * FROM user_data WHERE public_ip=?", (ip,))
+    existing_data = cursor.fetchone()
+
+    if existing_data:
+        cursor.execute("""
+        UPDATE user_data SET
+            user_agent=?,
+            country=?,
+            open_ports=?,
+            descriptions=?,
+            banners=?
+        WHERE public_ip=?
+        """, (user_agent, country, open_ports, descriptions, banners, ip))
+    else:
+        cursor.execute("""
+        INSERT INTO user_data (
+            public_ip, user_agent, country, open_ports, descriptions, banners
+        ) VALUES (?, ?, ?, ?, ?, ?)
+        """, (ip, user_agent, country, open_ports, descriptions, banners))
+
+    conn.commit()
+
+def load_data_from_database(ip):
+    cursor.execute("SELECT * FROM user_data WHERE public_ip=?", (ip,))
+    return cursor.fetchone()
+
+
 def scan_open_ports(ip: str):
     nmap.scan(ip, arguments='-T5 --script=banner')
 
@@ -44,18 +90,15 @@ def scan_open_ports(ip: str):
                 banner = nmap[ip][proto][port].get('script', {}).get('banner', 'No banner')
                 data[port] = {'service': service, 'banner': banner}
 
-    print(f"Open ports and banners on {ip}: {data}")
-
     return data
 
-def generate_description(ip, network_datas):
+def generate_description(network_datas):
     descriptions = []
     for port, data in network_datas.items():
         service = data['service']
         banner = data['banner']
-        #text_input = f"Beskriv kort følgende netværksoplysninger for en person uden viden om netværk og IT-sikkerhed, og fokusér på, hvad personen kan gøre bedre med netværket:\n\nIP: {ip}\nPort: {port}\nService: {service}\nBanner: {banner}\n"
         
-        text_input = f"Provide a brief description in Danish for a person with no knowledge of networking and IT-security about the open ports found in their network, and what they can do better to secure it. Only return a single bullet point. Open ports data:\n Port: {port}\n Service: {service}\n Banner: {banner}\n"
+        text_input = f"Provide a brief description in Danish for a person with no knowledge of networking and IT-security about the open ports found in their network, and what they can do better to secure it. Only return a single sentence. Data:\n Port: {port}\n Service: {service}\n Banner: {banner}\n"
 
         response = openai.Completion.create(
             engine="text-davinci-003",
@@ -74,9 +117,10 @@ def generate_description(ip, network_datas):
 # Display the user's public IP, country flag, and user agent under tab1
 with tab1:
    st.header("IP data")
-   st.write(f"Public IP: {public_ip_address}")
-   st.write(f"Country: {country_name}")
-   st.write(f"User agent is: {streamlit_js_eval(js_expressions='window.navigator.userAgent', want_output = True, key = 'UA')}")
+   st.write(f"Offentlig IP-adresse: {public_ip_address}")
+   st.write(f"Land: {country_name}")
+   user_agent = streamlit_js_eval(js_expressions='window.navigator.userAgent', want_output = True, key = 'UA')
+   st.write(f"User agent: {user_agent}")
    st.image(f"https://flagsapi.com/{country_code}/flat/64.png", width=64)
 
 with tab2:
@@ -84,29 +128,46 @@ with tab2:
    scan_ip = st.text_input("Angiv en ip adresse du vil scanne for åbne porte", value=public_ip_address)
 
    if st.button("Kør"):
-      print("Kør knappen aktiveret")
-      # Set the target IP address and port range
-
       scan_output = scan_open_ports(scan_ip)
 
       if scan_output:
-         st.write("Open ports, services, and banners on", scan_ip)
-         #st.write(scan_output)
+         st.write("Åbne porte, bannere og services på: ", scan_ip)
          st.table(scan_output)
 
-         descriptions = generate_description(scan_ip, scan_output)
-         st.write("Beskrivelse af dit netværk:")
+         descriptions = generate_description(scan_output)
+         st.write("AI-genereret forbedringsmuligheder af dit netværk:")
 
          bullet_points = []
          for desc in descriptions:
             bullet_points.append(f"- {desc}")
 
          st.markdown("\n".join(bullet_points))
-         
+
+         save_data_to_database(public_ip_address, user_agent, country_name, str(scan_output), "\n".join(descriptions), "\n".join([data["banner"] for data in scan_output.values()]))
+
+
       else:
-         st.warning("No open ports found.")
+         st.warning("Ingen åbne porte fundet...")
 
+# Load and display data from the database when the application starts
+loaded_data = load_data_from_database(public_ip_address)
 
+if loaded_data:
+    with st.expander("Gammel data"):
+      ip, user_agent, country, open_ports, descriptions, banners = loaded_data
+      st.write(f"Offentlig IP-adresse: {ip}")
+      st.write(f"User agent: {user_agent}")
+      st.write(f"Land: {country}")
+
+      if open_ports:
+         st.write("Åbne porte, bannere og services på: ")
+         st.write(open_ports)
+         st.write("AI-genereret forbedringsmuligheder af dit netværk:")
+         st.write(descriptions)
+         st.write("Bannere:")
+         st.write(banners)
+      else:
+         st.write("Ingen åbne porte fundet i gammelt data...")
 
    
 with tab3:
